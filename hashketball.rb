2e976a0c5525027fc307eaae454481bd277df59f
Emirhan Kaplan <emskaplann@gmail.com>
2019-09-04 21:59:51 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -159,7 +159,105 @@ def player_numbers(team)
  return newarr
 end
 
+def player_stats(name)
+  hash=game_hash
+    x=0
+  if hash[:home][:players][name]
+    return hash[:home][:players][name]
+  else hash[:away][:players][name]
+    return hash[:away][:players][name]
+  end
+end
 
+def big_shoe_rebounds
+  hash=game_hash
+  newarr=Array.new
+  make_keys1=hash[:home][:players].keys
+  make_keys2=hash[:away][:players].keys
+  x=0
+  while x<make_keys1.length
+  newarr << hash[:home][:players][make_keys1[x]][:shoe]
+  newarr << hash[:away][:players][make_keys2[x]][:shoe]
+  x+=1
+  end
+  big_shoe=newarr.max
+  if ourname1=make_keys1.find{|num| hash[:home][:players][num][:shoe]==big_shoe}
+  return hash[:home][:players][ourname1][:rebounds]
+  elsif ourname2=make_keys2.find{|num| hash[:away][:players][num][:shoe]==big_shoe}
+  return hash[:away][:players][ourname2][:rebounds]
+  end
+end
+
+def most_points_scored
+  hash=game_hash
+  newarr=Array.new
+  make_keys1=hash[:home][:players].keys
+  make_keys2=hash[:away][:players].keys
+  x=0
+  while x<make_keys1.length
+    newarr << hash[:home][:players][make_keys1[x]][:points]
+    newarr << hash[:away][:players][make_keys2[x]][:points]
+    x+=1
+  end
+  most_points=newarr.max
+  if ourname1=make_keys1.find{|num| hash[:home][:players][num][:points]==most_points}
+    return ourname1
+  elsif  ourname2=make_keys2.find{|num| hash[:away][:players][num][:points]==most_points}
+    return ourname2
+  end
+end
+
+def winning_team
+  hash=game_hash
+  newarr=0
+  newarr2=0
+  make_keys1=hash[:home][:players].keys
+  make_keys2=hash[:away][:players].keys
+  x=0
+  while x<make_keys2.length
+  newarr += hash[:home][:players][make_keys1[x]][:points]
+  newarr2 += hash[:away][:players][make_keys2[x]][:points]
+  x+=1
+  end
+  if newarr>newarr2
+    return hash[:home][:team_name]
+  elsif newarr2>newarr
+    return hash[:away][:team_name]
+  end
+end
+
+def player_with_longest_name
+  hash=game_hash
+  newarr2=Array.new
+  newarr=hash[:home][:players].keys+hash[:away][:players].keys
+  x=0
+  while x<newarr.length
+    newarr2 << newarr[x].size
+  x+=1
+  end
+  longestname = newarr2.max
+  newarr.find{|name| name.size==longestname}
+end
+
+def long_name_steals_a_ton?
+  hash=game_hash
+  longest_name=player_with_longest_name
+  newarr=[]
+  make_keys1=hash[:home][:players].keys
+  make_keys2=hash[:away][:players].keys 
+  x=0
+  while x<make_keys1.length
+    newarr << hash[:home][:players][make_keys1[x]][:steals]
+    newarr << hash[:away][:players][make_keys2[x]][:steals]
+  x+=1
+  end
+  moststeal = newarr.max
+  if hash[:away][:players][longest_name][:steals]=moststeal
+    return true
+  else 
+    return false
+  end
+end
 
 
 
